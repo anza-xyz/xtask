46a4ac808909ebe1d47662c753a69020671a5260
Mircea Colonescu <mircea.colonescu@anza.xyz>
2026-02-05 14:02:04 -0500

    refactor: extract publish types into dedicated module
    refactor: add types module with re-exports
    
    refactor: migrate publish command to use types module
    
    refactor: consolidate re-exports in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -30,12 +30,11 @@
 //! ```
 
 pub mod commands;
+pub mod types;
 pub mod utils;
 
-pub use commands::bump_version;
-pub use commands::publish;
-pub use commands::update_crate;
-
+pub use commands::{bump_version, publish, update_crate};
+pub use types::{PackageInfo, PublishOrderData};
 pub use semver::Version;
 
 pub type Result<T> = anyhow::Result<T>;
